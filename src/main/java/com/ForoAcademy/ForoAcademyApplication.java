d3d7c07a8efe1cffb5d592a9c6e37385d56a3264
dulceitamar <dulceitamar22@gmail.com>
2024-07-11 20:16:41 -0600

    Starting a new project

---
+++ src/main/java/com/ForoAcademy/ForoAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.ForoAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ForoAcademy/ForoAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForoAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
