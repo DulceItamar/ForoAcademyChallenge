09565d22bcebabbccbafb807f5f62924e33d488a
dulceitamar <dulceitamar22@gmail.com>
2024-07-12 13:58:21 -0600

    Feat: Doing STATELESS configuration

--- src/main/java/com/ForoAcademy/domain/user/UserRepository.java
+++ src/main/java/com/ForoAcademy/domain/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ForoAcademy.domain.user;
 
+import com.ForoAcademy.domain.academyMember.MemberCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String username);
 }
